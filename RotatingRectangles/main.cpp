7104ae5b6be6ccb3786e0fe96edcbe6b0fd9f79c
TomaszBaszuro <tomasz.baszuro@gmail.com>
2022-09-16 14:41:26 +0200

    Four rotating rectangles

--- RotatingRectangles/main.cpp
+++ RotatingRectangles/main.cpp
@@ -11,25 +11,16 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "RotatingRectangles", NULL, NULL);
     if (window == NULL)
     {
@@ -40,22 +31,18 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // build and compile our shader zprogram
-    // ------------------------------------
-    Shader ourShader("5.1.transform.vs.txt", "5.1.transform.fs.txt");
+    Shader Shader1("vertexShader.txt", "fragmentShader.txt");
+    Shader Shader2("vertexShader.txt", "fragmentShader.txt");
+    Shader Shader3("vertexShader.txt", "fragmentShader.txt");
+    Shader Shader4("vertexShader.txt", "fragmentShader.txt");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        // positions      
+    float vertices[] = {     
          0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
@@ -63,8 +50,8 @@ int main()
     };
 
     unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+        0, 1, 3, 
+        1, 2, 3
     };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -79,75 +66,99 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
-
-
-    // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
         processInput(window);
 
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        ourShader.use();
+        //rectangle #1
+        Shader1.use();
 
-        int vertexColorLocation = glGetUniformLocation(ourShader.ID, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, 1.0, 0.0f, 1.0f);
+        int vertexColorLocation1 = glGetUniformLocation(Shader1.ID, "ourColor");
+        glUniform4f(vertexColorLocation1, 0.0f, 1.0, 0.0f, 1.0f);
 
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.65, 0.65, 0.65));
+        glm::mat4 trans1 = glm::mat4(1.0f);
+        trans1 = glm::translate(trans1, glm::vec3(0.5, -0.5, 0.0));
+        trans1 = glm::rotate(trans1, (float)glfwGetTime(), glm::vec3(0.0, 0.5, -1.0));
+        trans1 = glm::scale(trans1, glm::vec3(0.65, 0.65, 0.65));
 
-        // render container
-        ourShader.use();
+        unsigned int transformLoc1 = glGetUniformLocation(Shader1.ID, "transform");
+        glUniformMatrix4fv(transformLoc1, 1, GL_FALSE, glm::value_ptr(trans1));
 
-        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-        glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        //rectangle #2
+        Shader2.use();
+
+        int vertexColorLocation2 = glGetUniformLocation(Shader2.ID, "ourColor");
+        glUniform4f(vertexColorLocation2, 1.0f, 0.0, 0.0f, 1.0f);
+
+        glm::mat4 trans2 = glm::mat4(1.0f);
+        trans2 = glm::translate(trans2, glm::vec3(-0.5, 0.5, 0.0));
+        trans2 = glm::rotate(trans2, (float)glfwGetTime(), glm::vec3(0.0, -0.5, -1.0));
+        trans2 = glm::scale(trans2, glm::vec3(0.65, 0.65, 0.65));
+
+        unsigned int transformLoc2 = glGetUniformLocation(Shader2.ID, "transform");
+        glUniformMatrix4fv(transformLoc2, 1, GL_FALSE, glm::value_ptr(trans2));
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //rectangle #3
+        Shader3.use();
+
+        int vertexColorLocation3 = glGetUniformLocation(Shader3.ID, "ourColor");
+        glUniform4f(vertexColorLocation3, 0.0f, 0.0, 1.0f, 1.0f);
+
+        glm::mat4 trans3 = glm::mat4(1.0f);
+        trans3 = glm::translate(trans3, glm::vec3(-0.5, -0.5, 0.0));
+        trans3 = glm::rotate(trans3, (float)glfwGetTime(), glm::vec3(-0.5, 0.0, -1.0));
+        trans3 = glm::scale(trans3, glm::vec3(0.65, 0.65, 0.65));
+
+        unsigned int transformLoc3 = glGetUniformLocation(Shader3.ID, "transform");
+        glUniformMatrix4fv(transformLoc3, 1, GL_FALSE, glm::value_ptr(trans3));
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //rectangle #4
+        Shader4.use();
+
+        int vertexColorLocation4 = glGetUniformLocation(Shader4.ID, "ourColor");
+        glUniform4f(vertexColorLocation2, 1.0f, 1.0, 0.0f, 1.0f);
+
+        glm::mat4 trans4 = glm::mat4(1.0f);
+        trans4 = glm::translate(trans4, glm::vec3(0.5, 0.5, 0.0));
+        trans4 = glm::rotate(trans4, (float)glfwGetTime(), glm::vec3(0.5, 0.0, -1.0));
+        trans4 = glm::scale(trans4, glm::vec3(0.65, 0.65, 0.65));
+
+        unsigned int transformLoc4 = glGetUniformLocation(Shader4.ID, "transform");
+        glUniformMatrix4fv(transformLoc4, 1, GL_FALSE, glm::value_ptr(trans4));
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
