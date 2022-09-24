d64415543c413c30dd802c7b7a51bd59187155c9
TomaszBaszuro <tomasz.baszuro@gmail.com>
2022-09-24 15:42:02 +0200

    Drawing one rotating rectangle via SSBO and glDrawElementsInstanced

--- RotatingRectangles/main.cpp
+++ RotatingRectangles/main.cpp
@@ -37,10 +37,7 @@ int main()
         return -1;
     }
 
-    Shader Shader1("vertexShader.txt", "fragmentShader.txt");
-    Shader Shader2("vertexShader.txt", "fragmentShader.txt");
-    Shader Shader3("vertexShader.txt", "fragmentShader.txt");
-    Shader Shader4("vertexShader.txt", "fragmentShader.txt");
+    Shader Shader("vertexShader.txt", "fragmentShader.txt");
 
     float vertices[] = {     
          0.5f,  0.5f, 0.0f,
@@ -65,6 +62,10 @@ int main()
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    GLuint SSBO;
+    glGenBuffers(1, &SSBO);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, SSBO);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -73,72 +74,23 @@ int main()
     {
         processInput(window);
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //rectangle #1
-        Shader1.use();
+        Shader.use();
 
-        int vertexColorLocation1 = glGetUniformLocation(Shader1.ID, "ourColor");
-        glUniform4f(vertexColorLocation1, 0.0f, 1.0, 0.0f, 1.0f);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, SSBO);
+       
+        glm::vec4 color = glm::vec4(0.5f,0.2f,0.7f,1.0f);
+        glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(color)+sizeof(glm::mat4), glm::value_ptr(color), GL_DYNAMIC_DRAW);
 
-        glm::mat4 trans1 = glm::mat4(1.0f);
-        trans1 = glm::translate(trans1, glm::vec3(0.5, -0.5, 0.0));
-        trans1 = glm::rotate(trans1, (float)glfwGetTime(), glm::vec3(0.0, 0.5, -1.0));
-        trans1 = glm::scale(trans1, glm::vec3(0.65, 0.65, 0.65));
+        glm::mat4 transformation = glm::mat4(1.0f);
+        transformation = glm::translate(transformation, glm::vec3(-0.5, 0.5, 0.5));
+        transformation = glm::rotate(transformation, (float)glfwGetTime(), glm::vec3(0.0, 0.0, -1.0));
+        transformation = glm::scale(transformation, glm::vec3(0.65, 0.65, 0.65));
+        glBufferSubData(GL_SHADER_STORAGE_BUFFER, sizeof(color), sizeof(transformation), glm::value_ptr(transformation));
 
-        unsigned int transformLoc1 = glGetUniformLocation(Shader1.ID, "transform");
-        glUniformMatrix4fv(transformLoc1, 1, GL_FALSE, glm::value_ptr(trans1));
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        //rectangle #2
-        Shader2.use();
-
-        int vertexColorLocation2 = glGetUniformLocation(Shader2.ID, "ourColor");
-        glUniform4f(vertexColorLocation2, 1.0f, 0.0, 0.0f, 1.0f);
-
-        glm::mat4 trans2 = glm::mat4(1.0f);
-        trans2 = glm::translate(trans2, glm::vec3(-0.5, 0.5, 0.0));
-        trans2 = glm::rotate(trans2, (float)glfwGetTime(), glm::vec3(0.0, -0.5, -1.0));
-        trans2 = glm::scale(trans2, glm::vec3(0.65, 0.65, 0.65));
-
-        unsigned int transformLoc2 = glGetUniformLocation(Shader2.ID, "transform");
-        glUniformMatrix4fv(transformLoc2, 1, GL_FALSE, glm::value_ptr(trans2));
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        //rectangle #3
-        Shader3.use();
-
-        int vertexColorLocation3 = glGetUniformLocation(Shader3.ID, "ourColor");
-        glUniform4f(vertexColorLocation3, 0.0f, 0.0, 1.0f, 1.0f);
-
-        glm::mat4 trans3 = glm::mat4(1.0f);
-        trans3 = glm::translate(trans3, glm::vec3(-0.5, -0.5, 0.0));
-        trans3 = glm::rotate(trans3, (float)glfwGetTime(), glm::vec3(-0.5, 0.0, -1.0));
-        trans3 = glm::scale(trans3, glm::vec3(0.65, 0.65, 0.65));
-
-        unsigned int transformLoc3 = glGetUniformLocation(Shader3.ID, "transform");
-        glUniformMatrix4fv(transformLoc3, 1, GL_FALSE, glm::value_ptr(trans3));
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        //rectangle #4
-        Shader4.use();
-
-        int vertexColorLocation4 = glGetUniformLocation(Shader4.ID, "ourColor");
-        glUniform4f(vertexColorLocation2, 1.0f, 1.0, 0.0f, 1.0f);
-
-        glm::mat4 trans4 = glm::mat4(1.0f);
-        trans4 = glm::translate(trans4, glm::vec3(0.5, 0.5, 0.0));
-        trans4 = glm::rotate(trans4, (float)glfwGetTime(), glm::vec3(0.5, 0.0, -1.0));
-        trans4 = glm::scale(trans4, glm::vec3(0.65, 0.65, 0.65));
-
-        unsigned int transformLoc4 = glGetUniformLocation(Shader4.ID, "transform");
-        glUniformMatrix4fv(transformLoc4, 1, GL_FALSE, glm::value_ptr(trans4));
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, 4);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -147,6 +99,7 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &SSBO);
 
     glfwTerminate();
     return 0;
